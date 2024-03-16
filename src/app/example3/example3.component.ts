cebe3fce1f1a5bd8a0b6ec2600c852b32d9ccf74
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2024-03-16 17:18:54 -0600

    clase2

---
+++ src/app/example3/example3.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-example3',
+  template: `
+    <p>
+      example3 works!
+    </p>
+  `,
+  styles: ``
+})
+export class Example3Component {
+
+}
