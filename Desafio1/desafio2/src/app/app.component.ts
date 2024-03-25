e04f39ff2d2441c1111f32ba98465d270546415d
Adei Cabañas <adeicabanas@MacBook-Air-de-Adei-2.local>
2024-03-24 18:58:08 -0600

    desafio2

--- Desafio1/desafio2/src/app/app.component.ts
+++ Desafio1/desafio2/src/app/app.component.ts
@@ -3,8 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'desafio2';
+  hasError = true;
+  students = [
+    { nombre: 'Juan', approved: false },
+    { nombre: 'Samantha', approved: false },
+    { nombre: 'Luna', approved: true },
+    { nombre: 'Franco', approved: true },
+    { nombre: 'Adei', approved: false },
+  ];
+
+  //metodo
+  alternarError() {
+    this.hasError = !this.hasError;
+  }
 }
